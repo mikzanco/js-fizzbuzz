79c1c38e44aa11b472d701a9e3a68d4a054bd8f3
mikzanco <mikyzancolo97@gmail.com>
2022-10-13 17:17:52 +0200

    fix ciclo e aggiunto colore per colorare poi i quadrati

--- js/script.js
+++ js/script.js
@@ -4,11 +4,13 @@ const container = document.querySelector('.container');
 
 const limit = 100;
  
-for (let i = 1; i <= limit; i++) {
+for (let i = 0; i <= limit; i++) {
     // creo il box 
     const box = document.createElement('div');
     box.className = 'box';
-    // aggiungo la classe al box
     // scrivo dentro al box i numeri dati dal ciclo i 
+    box.innerHTML = i + 1;
     // lo aggiungo poi al container
+    container.append(box);
+    
 }
