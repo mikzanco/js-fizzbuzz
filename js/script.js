096ad07bc97d923e7e1206d538c747774554ed6f
mikzanco <mikyzancolo97@gmail.com>
2022-10-14 10:06:19 +0200

    fix code mollto più leeggero ora in js

--- js/script.js
+++ js/script.js
@@ -9,44 +9,42 @@ for(let i = 1; i <= limit; i++){
     const box = document.createElement('div');
     box.className = 'box';
     // aggiungo adesso il cambio colore e scritta se multiplo di 3
-    if(!(i % 3)){
-        box.classList.add('acqua')
-        
-        // box.innerHTML = fizz;
-    }
-    // aggiungo adesso il cambio colore e scritta se multiplo di 5
-    if(!(i % 5)){
-        box.classList.add('coral')
-    }
-    // aggiungo adesso il cambio colore e scritta se multiplo di 15
-    if(!(i % 15)){
-        box.classList.add('purple')
-    }
-    // posso anche provare a cambiare il colore nel caso semplice in cui entrambe le prime due sono vere in modo da non farlo con i multipli di 15 ma con sia (&&) i multipli di 3 che di 5
-
-    // NON FUNZIONA COSI?!
-    // if(!(i % 5)) && (!(i % 3)){
+    // metodo logico con il multiplo di 15
+    // if(!(i % 15)){
     //     box.classList.add('purple')
     // }
+    // aggiungo adesso il cambio colore e scritta se multiplo di 5
+    
+    // aggiungo adesso il cambio colore e scritta se multiplo di 15
+    
+    // posso anche provare a cambiare il colore nel caso semplice in cui entrambe le prime due sono vere in modo da non farlo con i multipli di 15 ma con sia (&&) i multipli di 3 che di 5
+
+    // Adesso funziona
+    
     
     // scrivo dentro al box i numeri dati dal ciclo i 
     
-    box.innerHTML = i;
+    
 
     // aggiungo adesso il cambio scritta se multiplo di 5
-    if(!(i % 3)){
+    // messe cosi è megio pooichè mi evita di scrivere, ad esempio sul 15, prima il 3 poi sovrsacrivere il 5 e poi ancora sovrascrivere il 15
+    if(!(i % 15)){
+        box.classList.add('purple')
+        box.innerHTML = 'FizzBuzz';
+    }else if(!(i % 3)){
+        box.classList.add('acqua')
         box.innerHTML = 'Fizz';
-        
         // box.innerHTML = fizz;
+    }else if(!(i % 5)){
+        box.classList.add('coral')
+        box.innerHTML = 'Buzz';
+    }else{
+        box.innerHTML = i;
     }
     // aggiungo adesso il cambio scritta se multiplo di 5
-    if(!(i % 5)){
-        box.innerHTML = 'Buzz';
-    }
+    
     // aggiungo adesso il cambio patola al multiplo di 15
-    if(!(i % 15)){
-        box.innerHTML = 'FizzBuzz';
-    }
+    
     // lo aggiungo poi al container
     container.append(box);
     
